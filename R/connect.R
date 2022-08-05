70050270ea8f39459ef0b62503b30b7fe8eb541c
Rajesh Detroja <rajesh.detroja.93@gmail.com>
2022-08-05 15:38:34 -0400

    Create connect.R

---
+++ R/connect.R
@@ -0,0 +1,21 @@
+## Connect Rstudio and Github
+## Install some linux tools
+# sudo apt install curl
+# sudo apt install libcurl4-openssl-dev libssl-dev
+
+## Install library "usethis"
+install.packages("usethis")
+
+## Load library "usethis"
+library(usethis)
+
+## set your user name and email
+usethis::use_git_config(user.name = "Rajesh-Detroja", user.email = "rajesh.detroja.93@gmail.com")
+
+## set personal access token
+credentials::set_github_pat()
+
+## Restart R
+
+## Verify settings
+usethis::git_sitrep()
